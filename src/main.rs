96fd5a1fef09296dc3006583a2575034311e6351
pandaman64 <kointosudesuyo@infoseek.jp>
2022-07-09 05:58:08 +0900

    Introduce driver

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,28 @@
 use std::io;
 
-use chumsky::Parser;
+use chumsky::prelude::Simple;
 
-use crate::parser::LineMapper;
+use crate::{driver::Driver, opcode::Chunk, parser::LineMapper};
 
 mod ast;
 mod compiler;
+mod driver;
 mod opcode;
 mod parser;
 mod value;
 mod vm;
 
+fn print_errors(errors: Vec<Simple<char>>, mapper: &LineMapper) {
+    for error in errors.iter() {
+        eprintln!(
+            "error at line {}: {}",
+            mapper.find(error.span().start),
+            error
+        )
+    }
+}
+
 fn main() {
-    let arena = typed_arena::Arena::new();
     let source = r#"
 fun greet(first, last) {
     print("hello ");
@@ -30,29 +40,17 @@ print(
 );
 text = "foobar";
 print(text);
-"#;
+"#
+    .to_string();
     println!("source = {}", source);
-    let parser = parser::parser(&arena);
-    let mapper = LineMapper::new(source);
-    match parser.parse(source) {
-        Ok(ast) => {
-            let compiled = compiler::compile(ast, &mapper);
-            compiled.print();
 
-            let mut stdout = io::stdout().lock();
-            let mut vm = vm::Vm::new(compiled, &mut stdout);
-            while !vm.done() {
-                vm.step();
-            }
-        }
-        Err(errors) => {
-            for error in errors.iter() {
-                eprintln!(
-                    "error at line {}: {}",
-                    mapper.find(error.span().start),
-                    error
-                )
-            }
-        }
-    }
+    let mut stdout = io::stdout().lock();
+    let mut driver = Driver {
+        source,
+        run: true,
+        stdout: &mut stdout,
+        chunk_callback: Chunk::print,
+        error_callback: print_errors,
+    };
+    driver.run();
 }
