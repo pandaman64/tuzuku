1db7118f5105e5b1efedde115990b17950910cef
pandaman64 <kointosudesuyo@infoseek.jp>
2022-07-08 13:17:05 +0900

    Init flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "An interpreted language with continuations";
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    rec {
+      packages.x86_64-linux.tuzuku = pkgs.rustPlatform.buildRustPackage
+        rec {
+          pname = "tuzuku";
+          version = "0.1.0";
+
+          src = self;
+          cargoLock.lockFile = "${src}/Cargo.lock";
+        };
+      defaultPackage.x86_64-linux = packages.x86_64-linux.tuzuku;
+
+      devShell.x86_64-linux = pkgs.mkShell {
+        buildInputs = [
+          pkgs.nixpkgs-fmt
+        ];
+      };
+    };
+}
